168d433fe65bbd73c804b35bae814232a231a619
Admin <Admin@WIN-4B40LAG9JOG>
2021-12-22 23:52:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello_World___
+{
+    class Program
+    {/// <summary>
+    /// Код с которого начинает работать программа(точка входа)
+    /// </summary>
+    /// <param name="args">Параметр</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");//выводим текст 
+            Console.ReadKey();//ждем нажатия любой клавиши
+        }
+    }
+}
